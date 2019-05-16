87ea5bbf255d7513ac659ddf1b9dcba68837e987
HL <36248745+honestbonsai@users.noreply.github.com>
2019-05-16 11:13:30 -0400

    Init

---
+++ src/bug.js
@@ -0,0 +1,10 @@
+const Web3 = require("web3");
+const createNewBlock$ = require("@drizzle-utils/new-block-stream");
+
+const web3 = new Web3("https://mainnet.infura.io/v3/KEY"); // HttpProvider
+// track new blocks
+const { observable } = createNewBlock$({
+  web3,
+  pollingInterval: 200, // only used if non-WebsocketProvider
+});
+observable.subscribe(console.log)
